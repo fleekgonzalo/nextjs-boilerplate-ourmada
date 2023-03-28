4066d69e7ef742ef207e032d55d19c61af247869
fleekgonzalo <114927947+fleekgonzalo@users.noreply.github.com>
2023-03-28 14:13:36 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,14 @@ const nextConfig = {
 }
 
 module.exports = {
+  
   trailingSlash: true,
+  
+   exportPathMap: async function () {
+    const paths = {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/api/hello': { page: '/api/hello' }, // server-side page
+    };
+  
 };
